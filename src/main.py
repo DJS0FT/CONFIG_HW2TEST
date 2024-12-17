a4a56e64c45ccc98d8cfc51a589c7453325e227f
Nikita Tokarev <gary9135@gmail.com>
2024-12-17 09:34:48 +0300

    Refactor main.py to use add function from utils.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
 def hello_world(): 
     print('Hello, World!') 
+from utils import add 
+ 
+def main(): 
+    result = add(5, 7) 
+    print(f'Result of add: {result}') 
+ 
+if __name__ == '__main__': 
+    main() 
